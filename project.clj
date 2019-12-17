345a4899941b7b2064c5d17c4d7f4c3640d157bc
Aaron Dixon <atdixon@gmail.com>
2019-12-16 21:39:19 -0600

    Minor

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.atdixon/thurber "0.0.1-alpha1-SNAPSHOT"
+(defproject com.github.atdixon/thurber "0.0.2-alpha1-SNAPSHOT"
   :author "Aaron Dixon <https://write.as/aaron-d/>"
   :description "thurber: Apache Beam on Clojure"
   :url "https://github.com/atdixon/thurber"
