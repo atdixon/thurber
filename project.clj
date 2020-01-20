f43569cc1d897bd2af8a58791c0acb739366d318
Aaron Dixon <atdixon@gmail.com>
2020-01-19 22:03:56 -0600

    Version 0.0.2-alpha9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.atdixon/thurber "0.0.2-alpha9-SNAPSHOT"
+(defproject com.github.atdixon/thurber "0.0.2-alpha9"
   :author "Aaron Dixon <https://write.as/aaron-d/>"
   :description "thurber: Apache Beam on Clojure"
   :url "https://github.com/atdixon/thurber"
