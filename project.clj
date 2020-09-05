dfcdda7f3eed87f64c25fb170fd331ccb8c7d817
Aaron Dixon <atdixon@gmail.com>
2020-09-05 11:52:54 -0500

    Version 0.0.7-alpha1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def version-apache-beam "2.23.0")
 
-(defproject com.github.atdixon/thurber "0.0.7"
+(defproject com.github.atdixon/thurber "0.0.7-alpha1-SNAPSHOT"
   :author "Aaron Dixon <https://write.as/aaron-d/>"
   :description "thurber: Apache Beam on Clojure"
   :url "https://github.com/atdixon/thurber"
